8f04962c8c7af5007887fe780c472705fc98cdc2
Veroni <73184907+Veroniclover@users.noreply.github.com>
2026-04-18 19:33:07 +0900

    Update stsup.sh

--- stsup.sh
+++ stsup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-set -euo pipefail
-
 # install packs
-pkg update -y && pkg upgrade -y
+pkg update -y
+pkg upgrade -y 
 pkg install termux-api -y
 
 # install the app
 curl -sL "https://github.com/Veroniclover/fprox/releases/download/v0.0.1/stsup" -o "${PREFIX}/bin/stsup"
-chmod +x  "${PREFIX}/bin/stsup"
+chmod +x "${PREFIX}/bin/stsup"
 
 # allow external apps
 sed -i 's/^# allow-external-apps = true/allow-external-apps = true/' ~/.termux/termux.properties
-termux-reload-settings
+termux-reload-settings || true
